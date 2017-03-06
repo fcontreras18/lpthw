7eb2646897e79c0bbd142948d3fff281583ecbf1
Francisco Echeverria <fcontreras18@gmail.com>
2017-03-05 22:27:08 -0800

    New folders

--- ex_10-19/ex13.py
+++ ex_10-19/ex13.py
@@ -6,3 +6,5 @@ print "The script is called:", script
 print "Your first variable is:", first
 print "Your second variable is:", second
 print "Your third variable is:", third
+color = raw_input("Which is your favorite name? ")
+print "So, your favorite name is %r" % color
